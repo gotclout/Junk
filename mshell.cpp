9935564d863411b3872d1671668e671c22551d8a
gotclout <rlfosterjr@gmail.com>
2014-07-21 11:25:10 -0400

    Update mshell.cpp

--- mshell.cpp
+++ mshell.cpp
@@ -1,6 +1,7 @@
 #include <wait.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <sstream>
 #include <iostream>
 #include <unistd.h>
 #include <stdlib.h>
@@ -21,6 +22,37 @@
 using namespace std;
 
 /**
+ * Contains data associated with a command to be executed
+ */
+struct CmdData
+{
+  char* args[MAX_LEN], *path, *inf, *outf; //args, path to exec io redirect
+  bool amp; //execute no wait
+  int argc; //num args
+
+  /**
+   * Default Constructor
+   */
+  CmdData()
+  {
+    memset(args, 0, sizeof(char*)*MAX_LEN);
+    argc = 0;
+    amp = 0; 
+    path = inf = outf = 0;
+  };
+
+  /**
+   * Retrieves the argument at the ith index
+   */
+  char* at(int i = 0) { return *args && i < argc ? args[i] : 0; };
+
+  /**
+   * Retrieves the jth character of the ith argument
+   */
+  char argat(int i = 0, int j = 0) { return at(i) ? *at(i) + j : '\0'; };
+};
+
+/**
  * Execute forks a process to execute the specified command
  *
  * @param char** args, the command and arguments
@@ -30,26 +62,23 @@ using namespace std;
  * @param bool amp, indicates whether or not the main proc should wait
  *  for the child proc or return immediately
  */
-bool execute(char** args, char* path, char* inf, char* outf, bool amp)
+bool execute(CmdData & cmd)
 {
   bool ret = true;
-  int fid, pid = fork();  //create child
-  if(pid < 0)
+  int fid, pid = fork();       //create child
+  if(pid < 0) ret = false;
+  if(!ret) cerr << "ERROR: could not fork subprocess\n";
+  else if(pid == 0)            //child process
   {
-    cerr << "ERROR: could not fork subprocess\n";
-    ret = false;          //exception
-  }
-  else if(pid == 0)       //child process
-  {
-    if(inf && !close(0))  fid = open(inf, O_RDONLY|O_NONBLOCK, 00222);
-    if(outf && !close(1)) fid = open(outf, O_WRONLY|O_RDONLY|O_CREAT, 00666);
-    execv(path, args); //execute command
-    if(inf) close(0);               //end redirection
-    if(outf) close(1);
-    perror(*args);
+    if(cmd.inf && !close(0)) fid=open(cmd.inf,O_RDONLY|O_NONBLOCK,00222);
+    if(cmd.outf && !close(1))fid=open(cmd.outf,O_WRONLY|O_RDONLY|O_CREAT,00666);
+    execv(cmd.path, cmd.args); //execute command
+    if(cmd.inf) close(0);      //end redirection
+    if(cmd.outf) close(1);
+    perror(*cmd.args);
     exit(0);
   }
-  else if(!amp) waitpid(pid, NULL, 0);
+  else if(!cmd.amp) waitpid(pid, NULL, 0);
   else usleep(50000);
   return ret;
 }
@@ -61,16 +90,17 @@ bool execute(char** args, char* path, char* inf, char* outf, bool amp)
  * @param bool errWarn indicates whether or not errors should be issued
  * @return int8_t 1 if ok, 0 if not executable -1 if DNE
  */
-int8_t canexec(const char* pPath, bool errWarn = true)
+int8_t canexec(const char* pPath, char err[])
 {
   int8_t rv = -1;
-  if(!access(pPath, F_OK))              //exists
+  memset(err, 0, MAX_LEN);
+  if(!access(pPath, F_OK))         //exists
   {
     rv++;
     if(!access(pPath, X_OK)) rv++; //executable
-    else if(errWarn) cerr << "ERROR: " << pPath << " is not executable\n";
+    else strcat(strncpy(err, pPath, strlen(pPath)), " is not executable\n");
   }
-  else if(errWarn) cerr << "ERROR: " << pPath << " does not exist\n";
+  else strcat(strncpy(err, pPath, strlen(pPath)), " does not exist\n");
   return rv;
 }
 
@@ -81,55 +111,51 @@ int8_t canexec(const char* pPath, bool errWarn = true)
  *  first entry is the command to be executed
  * @return char* is a path to the command to be executed (may be null)
  */
-bool get_cmd_path(char* args, char* & s_path)
+bool get_cmd_path(char* args, char* & cmdpth)
 {
-  int errno     = -1;
-  char *pathenv = getenv("PATH"),
-       *pwdenv  = getenv("PWD");
-  size_t plen   = strlen(pathenv), pwdlen = strlen(pwdenv);
-  s_path  = (char*) malloc(2*(MAX_LEN)*sizeof(char)); //path vairable
-  memset(s_path, 0, 2*MAX_LEN*sizeof(char));
+  char errstr[MAX_LEN];
+  int errno     = -1,
+      i         = 1;
+  char* pwdenv  = getenv("PWD");
+  size_t pwdlen = strlen(pwdenv), pthenvlen, cpylen;
+
+  cmdpth        = (char*) malloc(2*(MAX_LEN)*sizeof(char)); //path vairable
+  memset(cmdpth, 0, 2*MAX_LEN*sizeof(char));
   if(*args != '.' && *args != '/')         //    cmd
   {
-    char path[plen + 1];
-    strncpy(path, pathenv, plen);
+    char* pthenv = getenv("PATH");
+    char path[( pthenvlen = strlen(pthenv))+1];
+    memset(path, 0, MAX_LEN);
+    strncpy(path, pthenv, pthenvlen);
     for(char* p = strtok(path, ":"); p && errno == -1; p = strtok(0, ":"))
     {
-      if(!p) continue;
-      if(*p == '.') strcat(strcat(strncpy(s_path, pwdenv, pwdlen), "/"), args);
-      else strcat(strcat(strncpy(s_path, p, strlen(p)), "/"), args);
-      errno = canexec(s_path, false);
-      if(errno < 1) memset(s_path, 0, 2*MAX_LEN*sizeof(char));
+      if(*p == '.') strcat(strcat(strncpy(cmdpth, pwdenv, pwdlen), "/"), args);
+      else strcat(strcat(strncpy(cmdpth, p, strlen(p)), "/"), args);
+      errno = canexec(cmdpth, errstr);
+      if(errno == -1) memset(cmdpth, 0, 2*MAX_LEN*sizeof(char));
     }
-    if(errno == -1) cerr << "ERROR: " << args << " does not exist\n";
-    else if(errno == 0) cerr << "ERROR: " << s_path << " is not executable\n";
   }
   else
   {
-    if(*args == '/') strcat(s_path, args);  //   /cmd
-    else if(*args == '.' && *args+1 == '/')
-    {
-        strncpy(s_path, pwdenv, pwdlen);
-        strcat(s_path, args+1);             //  ./cmd
-    }
-    else if(*args == '.' && *args+1 == '.')
-    {
-      strncpy(s_path, pwdenv, pwdlen - strlen(strrchr(pwdenv, '/')));
-      strcat(s_path, args+2);               // ../cmd
-    }
-    errno = canexec(s_path);
+    if(*args == '/') strcat(cmdpth, args);  //   /cmd
+    else if(*args == '.' && *args+i == '/') cpylen = pwdlen;
+    else if(*args+i++ == '.') cpylen = pwdlen - strlen(strrchr(pwdenv, '/'));
+    else ++i;
+    if(i < 3) strcat(strncpy(cmdpth, pwdenv, cpylen), args+i);
+    errno = canexec(cmdpth, errstr);
   }
+  if(errno < 1) cerr << "ERROR: " << errstr << '\n';
   return errno == 1;
 }
 
 /**
- * counts and parses the command line into args
+ * Counts and parses the command line into args
  *
  * @param char** args, the string array of parsed arguments
  * @param int num_args, the num arguments in args
  * @return bool true if success, false otherwise
  */
-bool parse_args(char** args, int & argc)
+bool parse_args(char* args[], int & argc)
 {
   bool rv = true;
   char* buf = 0, *bptr = 0; //save buf so that it can be deallocated
@@ -165,34 +191,22 @@ bool parse_args(char** args, int & argc)
  */
 int main(int argc, char** argv)
 {
-  char* args[MAX_LEN]      ={0};         //parsed command list
-  char* path               = NULL;       //parsed path
-  char* inf                = NULL;       //redirection input file name
-  char* outf               = NULL;       //redirection output file name
-  bool amp = 0;                          //& flag
   int i = 0, pos;
-
-  argc = 0;
-  parse_args(args, argc); //parse command line
-  while((strcmp(args[0], "exit"))) //loop until exit
+  CmdData cmd, *cmdp = &cmd;
+  while(parse_args(cmd.args, cmd.argc) && strcmp(cmdp->args[0], "exit") != 0)
   {
-    for(i = 0, pos = 0; i < argc; ++i) //check for ampersand and redirection
+    for(i = 0, pos = 0; i < cmd.argc; ++i) //check for & < > flags
     {
-      if(*args[i] == '<') inf = args[++i];
-      else if(*args[i] == '>') outf = args[++i];
-      else if(*args[i] == '&')amp = true;
-      else if(!inf && !outf && !amp)++pos;
+      if(cmd.argat(i,0) == '<') cmd.inf = cmd.at(++i);
+      else if(cmd.argat(i,0) == '>') cmd.outf = cmd.at(++i);
+      else if(cmd.argat(i,0) == '&') cmd.amp = true;
+      else if(!cmd.inf && !cmd.outf && !cmd.amp)++pos;
     }
-    if(args[pos]) args[pos] = 0;
-    if(get_cmd_path(*args, path)) execute(args, path, inf, outf, amp);
-    free(path);
-    path = 0;
-    for(i = 0; i < argc; ++i) if(args[i]) free(args[i]); //free args
-    argc = 0;                                            //reset
-    inf = outf = NULL;
-    parse_args(args, argc);                              //repeat
+    if(pos && cmd.at(pos)) cmd.args[pos] = 0;
+    if(get_cmd_path(*cmd.args, cmd.path)) execute(cmd);
+    free(cmd.path);
+    for(i = 0; i < cmd.argc; ++i) if(cmd.at(i)) free(cmd.at(i));  //free args
   }
-  for(i = 0; i < argc; ++i) if(args[i]) free(args[i]);
 
   return 0;
 }
