d2b9bafed52c613a5f316c6d83a6b3d99c657e11
gotclout <rlfosterjr@gmail.com>
2014-08-01 13:40:16 -0400

    Update strings.cpp

--- strings.cpp
+++ strings.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
+#include <iomanip>
 #include <sstream>
 #include <string>
 #include <map>
 
+#define align cout << setw(16) << setfill(' ')
+
 using namespace std;
 
 const static string str     =
-  "The quick brown fox jumps swiftly over the lazy brown dog at night\n";
+  "The quick brown fox jumps swiftly over the lazy brown dog at night";
 const static string uvowels =
   "ieayiiieaaaouyeeeeyayayayaiiiaaaeooouuuuuuuuaeiouy";
 const static string vowels  = "aeiouy";
@@ -14,7 +17,6 @@ const static string vowels  = "aeiouy";
 map<char, string> v;
 map<char, string>::iterator vIt;
 
-stringstream sstr;
 
 /**
  * Display the first and last segments of str containing s
@@ -23,12 +25,12 @@ void segment(const string & s)
 {
   size_t pos = str.find_first_of(s);
   string val = str.substr(0, pos + s.size());
-  cout << "Start Segment: " << val << '\n';
+  align << right << "Start Segment: " << val << '\n';
   pos = str.find_last_of(s);
   val = str.substr(pos, str.size() - pos);
   pos = str.rfind(s);
   val = str.substr(pos, str.size() - pos);
-  cout << "  End Segment: " << val << '\n';
+  align << right << "End Segment: " << val << endl << endl;
 }
 
 /**
@@ -36,47 +38,39 @@ void segment(const string & s)
  */
 string sortvowels()
 {
-  string retVal = "    Vowel Map: ";
-  int i = 0;
-  for( ; i < uvowels.size(); ++i) v[uvowels[i]] += uvowels[i];
-  for(i = 0; i < vowels.size(); ++i)
+  align << right << "Vowel Map: " << endl;
+  for(size_t i = 0; i < uvowels.size(); ++i) v[uvowels[i]] += uvowels[i];
+  for(size_t i = 0; i < vowels.size(); ++i)
   {
-    retVal += v[vowels[i]];
-    if(v[vowels[i]] != v[vowels[i+1]]) retVal += "\n               ";
+    if(v[vowels[i]] != v[vowels[i+1]])
+      align << left << ' ' << v[vowels[i]] << '\n';
   }
-  return retVal;
+
+  return "";
 }
 
 /**
  * Print the specified vowel in v
  */
-string printavowel(const char & c)
+string printvowel(const char & c)
 {
-  string retVal = "";
-
   for(vIt = v.begin(); vIt != v.end(); ++vIt)
-  {
-    if(vIt->first == c)
-    {
-      retVal += vIt->second + "\n";
-      break;
-    }
-  }
-
-  return retVal;
+    if(vIt->first == c) return vIt->second;
+  stringstream sstr;
+  sstr << c << " is not a vowel\n";
+  return sstr.str();
 }
 
 /**
  * Find string s in str
  */
-size_t find(const string & s)
+size_t find(const string & s, string sstr = str)
 {
-  sstr << "   Search Str: " <<  s << "\n   Source Str: " <<  str
-       << "     Position: ";
-  size_t pos = str.find(s);
-  sstr << pos;
-  if(pos != string::npos) cout << sstr.str() + "\n";
-  else cout << sstr.str() << "DNE\n";
+  align << right << endl << "Search Str: " << s << endl;
+  size_t pos = sstr.find(s);
+  align << right << "Source Str: " << sstr << endl;
+  if(pos == string::npos) align << right << "Position: " << "DNE" << endl;
+  else align << right << "Position: " << pos << endl;
 
   return pos;
 }
@@ -84,14 +78,32 @@ size_t find(const string & s)
 /**
  * Find and replace the string s with r in str
  */
-string replace(const string & s, const string & r)
+string replace(const string & s, const string & r, string sstr = str)
 {
-  cout << "\n  Replace Str: " << r << '\n';
-  string tmp = str;
-  size_t pos = find(s);
-
+  align << right << "Replace Str: " << r;
+  string tmp = sstr;
+  size_t pos = find(s, tmp);
   if(pos != string::npos) tmp = tmp.replace(pos, s.size(), r);
-  cout << "   Result Str: " <<  tmp;
+  align << right << "Result Str: " << tmp << endl << endl;
+
+  return tmp;
+}
+
+/**
+ * Find and replace all instances of s with r in str
+ */
+string replaceall(const string & s, const string & r, string sstr = str)
+{
+  align << right << "Replace All: " << endl;
+  align << right << "Replace Str: " << r;
+  string tmp = sstr;
+  size_t pos = find(s, tmp);
+  while(pos != string::npos)
+  {
+    tmp = tmp.replace(pos, s.size(), r);
+    pos = find(s, tmp);
+  }
+  align << right << "Result Str: " << tmp << endl << endl;
 
   return tmp;
 }
@@ -101,21 +113,27 @@ string replace(const string & s, const string & r)
  */
 int main(int argc, char** argv)
 {
-  const string s = "brown",
-               r = "orange";
+  const string s = "brown", r = "orange", e = "yellow";
+  size_t sz = vowels.size();
 
-  cout << "      Segment:\n";
+  align << right << "Segment: " << endl;
   segment(s);
-  cout << "         Find:\n";
+  align << right << "Find: ";
+  find(e);
   find(s);
-  cout << "\n      Replace:";
-  replace(s, r);
-  cout << "\n         Sort:\n" << sortvowels() << "\n        Print:\n       Vowels: ";
-  string (*pfuncptr) (const char &) = &printavowel;
-  cout << (*pfuncptr) ('a');
-  for(int i = 1; i < 6; ++i) cout << "               " <<  printavowel(vowels[i]);
-  cout << "\nfin\n";
+  align << right << endl << "Replace: " << endl;
+  string rs = replace(s, r);
+  rs = replace(s, r, rs);
+  replace(s, r, rs);
+  replaceall(s, r);
+  align << right << "Sort: " << endl;
+  align << right << sortvowels() << endl;
+  align << "Print Vowels: " << endl;
+  string (*pfuncptr) (const char &) = &printvowel;
+  align << left << ' ' << (*pfuncptr) ('a') << endl;
+  for(int i = 1; i < sz; ++i)
+    align << left << ' ' << printvowel(vowels[i]) << endl;
+  align << left << ' ' << printvowel('z') << "\nfin\n";
 
   return 0;
 }
-
