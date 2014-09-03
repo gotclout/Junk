ef262c6a0814d05e6a1bf7b0af07e3b6389eb1d4
gotclout <rlfosterjr@gmail.com>
2014-09-03 17:08:53 -0400

    Create ah1.cpp
    todo

---
+++ ah1.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+const int n = 7;
+
+int partition_r(int A[], const int x, const int sz = 0)
+{
+  int m = (sz / 2) + 1;
+
+  if(A[m-1] <= x && A[m+1] > x)
+    return m;
+  else if(m < 1)
+    return 0;
+  else if(m > n - 1)
+    return A[n-1] + 1;
+  else if(A[m] <= x)
+  {
+    m = ((m + 1) / 2) + 1 ;
+  }
+  else
+  {
+    m = ((m - 1) / 2) + 1;
+  }
+  return partition_r(A, A[m], m);
+
+}
+
+int partition(int A[], const int x)
+{
+  int m = -1,
+      i = 1;
+
+  cout << "A: ";
+  for( ; i <= n; ++i)
+  {
+    (i + 1) == n ? cout << A[i] << endl : cout << A[i] << " ";
+  }
+
+
+  m = (n / 2) + 1;
+
+  while(!(A[m] <= x && A[m+1] > x) && m > 0 && m < n )
+  {
+    if(m < 1)
+    {
+      m = 0;
+    }
+    else if(m > n - 1)
+    {
+      m = A[n - 1] + 1;
+    }
+    else if(A[m] <= x)
+    {
+      m = ((m + 1) / 2) + 1 ;
+      cout << "m: " << m << endl;
+    }
+    else if(m < 2)
+    {
+      m = ((m - 1) / 2) + 1;
+      cout << "m: " << m << endl;
+    }
+    else m = 0;
+  }
+  return m;
+}
+
+int main(int argv, char** argc)
+{
+  int A[7] = {-1, 2, 4, 5, 5, 8, 9};
+  int q    =
+      partition(A, 5);
+  cout << "Partition Returned Q: " << q << endl; //4
+  q = partition(A, 6);
+  cout << "Partition Returned Q: " << q << endl;
+  q = partition(A, 7);
+  cout << "Partition Returned Q: " << q << endl;
+  q = partition(A, 4);
+  cout << "Partition Returned Q: " << q << endl; //2
+  q = partition(A, 3);
+  cout << "Partition Returned Q: " << q << endl; //1
+  q = partition(A, 2);
+  cout << "Partition Returned Q: " << q << endl;
+  q = partition(A, 1);
+  cout << "Partition Returned Q: " << q << endl; //0
+
+  q = partition_r(A, 5, 7);
+  cout << "Partition R Returned Q: " << q << endl;
+  q = partition_r(A, 6, 7);
+  cout << "Partition R Returned Q: " << q << endl;
+  q = partition_r(A, 7, 7);
+  cout << "Partition R Returned Q: " << q << endl;
+  q = partition_r(A, 4, n);
+  cout << "Partition R Returned Q: " << q << endl;/*
+  q = partition_r(A, 3);
+  cout << "Partition R Returned Q: " << q << endl;
+  q = partition_r(A, 2);
+  cout << "Partition R Returned Q: " << q << endl;*/
+
+  return 1;
+}
+
