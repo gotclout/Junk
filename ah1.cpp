f1b19e76ea04440654257942d432c5b00e9f7371
gotclout <rlfosterjr@gmail.com>
2014-09-03 23:35:16 -0400

    Update ah1.cpp

--- ah1.cpp
+++ ah1.cpp
@@ -8,23 +8,18 @@ const int n = 7;
 int partition_r(int A[], const int x, const int sz = 0)
 {
   int m = (sz / 2) + 1;
-
-  if(A[m-1] <= x && A[m+1] > x)
+  cout << "x : " << x << " sz : " << sz << " m : " << m << endl;
+  if(m == 1 && sz == 0 && A[m] > x) return 0;
+  else if(m < n && (A[m-1] <= x && A[m+1] > x))
     return m;
-  else if(m < 1)
-    return 0;
+  //else if ( sz == 0 && x == 1) return 0;
   else if(m > n - 1)
     return A[n-1] + 1;
   else if(A[m] <= x)
-  {
     m = ((m + 1) / 2) + 1 ;
-  }
   else
-  {
     m = ((m - 1) / 2) + 1;
-  }
   return partition_r(A, A[m], m);
-
 }
 
 int partition(int A[], const int x)
@@ -33,35 +28,22 @@ int partition(int A[], const int x)
       i = 1;
 
   cout << "A: ";
-  for( ; i <= n; ++i)
-  {
+  for( ; i < n; ++i)
     (i + 1) == n ? cout << A[i] << endl : cout << A[i] << " ";
-  }
-
 
   m = (n / 2) + 1;
 
   while(!(A[m] <= x && A[m+1] > x) && m > 0 && m < n )
   {
-    if(m < 1)
-    {
+    if(m == 1)
       m = 0;
-    }
     else if(m > n - 1)
-    {
-      m = A[n - 1] + 1;
-    }
+      m = n - 1;
     else if(A[m] <= x)
-    {
-      m = ((m + 1) / 2) + 1 ;
-      cout << "m: " << m << endl;
-    }
-    else if(m < 2)
-    {
+          m = ((m + 1) / 2) + 1 ;
+    else// if(m < 2)
       m = ((m - 1) / 2) + 1;
-      cout << "m: " << m << endl;
-    }
-    else m = 0;
+    cout << "m: " << m << endl;
   }
   return m;
 }
@@ -69,8 +51,9 @@ int partition(int A[], const int x)
 int main(int argv, char** argc)
 {
   int A[7] = {-1, 2, 4, 5, 5, 8, 9};
+  cout << "begin iterative" << endl;
   int q    =
-      partition(A, 5);
+    partition(A, 5);
   cout << "Partition Returned Q: " << q << endl; //4
   q = partition(A, 6);
   cout << "Partition Returned Q: " << q << endl;
@@ -85,6 +68,7 @@ int main(int argv, char** argc)
   q = partition(A, 1);
   cout << "Partition Returned Q: " << q << endl; //0
 
+  cout << "begin recurssive" << endl;
   q = partition_r(A, 5, 7);
   cout << "Partition R Returned Q: " << q << endl;
   q = partition_r(A, 6, 7);
@@ -92,12 +76,15 @@ int main(int argv, char** argc)
   q = partition_r(A, 7, 7);
   cout << "Partition R Returned Q: " << q << endl;
   q = partition_r(A, 4, n);
-  cout << "Partition R Returned Q: " << q << endl;/*
+  cout << "Partition R Returned Q: " << q << endl;
   q = partition_r(A, 3);
   cout << "Partition R Returned Q: " << q << endl;
   q = partition_r(A, 2);
-  cout << "Partition R Returned Q: " << q << endl;*/
+  cout << "Partition R Returned Q: " << q << endl;
+  q = partition_r(A, 1);
+  cout << "Partition R Returned Q: " << q << endl;
 
   return 1;
 }
 
+                                           
