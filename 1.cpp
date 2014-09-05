a44d76140aab894cb79ca15c1873345b1aac2476
gotclout <rlfosterjr@gmail.com>
2014-09-04 21:43:50 -0400

    Update 1.cpp
    output
    
    Begin...
    A: 2 4 5 5 8 9
    
    Testing Partition...
    RPartioning...
    IPartioning...
    
    RPartition 1 Returned Q: 0
    
    IPartition 1 Returned Q: 0
    RPartioning...
    IPartioning...
    
    RPartition 2 Returned Q: 1
    
    IPartition 2 Returned Q: 1
    RPartioning...
    IPartioning...
    
    RPartition 3 Returned Q: 1
    
    IPartition 3 Returned Q: 1
    RPartioning...
    IPartioning...
    
    RPartition 4 Returned Q: 2
    
    IPartition 4 Returned Q: 2
    RPartioning...
    IPartioning...
    
    RPartition 5 Returned Q: 4
    
    IPartition 5 Returned Q: 4
    RPartioning...
    IPartioning...
    
    RPartition 6 Returned Q: 4
    
    IPartition 6 Returned Q: 4
    RPartioning...
    IPartioning...
    
    RPartition 7 Returned Q: 4
    
    IPartition 7 Returned Q: 4
    RPartioning...
    IPartioning...
    
    RPartition 8 Returned Q: 5
    
    IPartition 8 Returned Q: 5
    RPartioning...
    IPartioning...
    
    RPartition 9 Returned Q: 6
    
    IPartition 9 Returned Q: 6
    RPartioning...
    IPartioning...
    
    RPartition 10 Returned Q: 6
    
    IPartition 10 Returned Q: 6
    End...
    Begin...
    A: 1 1 5 5 7 7 9
    
    Testing Partition...
    RPartioning...
    IPartioning...
    
    RPartition 1 Returned Q: 2
    
    IPartition 1 Returned Q: 1
    RPartioning...
    IPartioning...
    
    RPartition 2 Returned Q: 2
    
    IPartition 2 Returned Q: 2
    RPartioning...
    IPartioning...
    
    RPartition 3 Returned Q: 2
    
    IPartition 3 Returned Q: 2
    RPartioning...
    IPartioning...
    
    RPartition 4 Returned Q: 2
    
    IPartition 4 Returned Q: 2
    RPartioning...
    IPartioning...
    
    RPartition 5 Returned Q: 4
    
    IPartition 5 Returned Q: 4
    RPartioning...
    IPartioning...
    
    RPartition 6 Returned Q: 4
    
    IPartition 6 Returned Q: 4
    RPartioning...
    IPartioning...
    
    RPartition 7 Returned Q: 6
    
    IPartition 7 Returned Q: 6
    RPartioning...
    IPartioning...
    
    RPartition 8 Returned Q: 6
    
    IPartition 8 Returned Q: 6
    RPartioning...
    IPartioning...
    
    RPartition 9 Returned Q: 7
    
    IPartition 9 Returned Q: 7
    RPartioning...
    IPartioning...
    
    RPartition 10 Returned Q: 7
    
    IPartition 10 Returned Q: 7
    End...

--- 1.cpp
+++ 1.cpp
@@ -3,15 +3,9 @@
 
 using namespace std;
 
-
-int apartition(int A[], int x, int lo, int hi)
+int r_partition(int A[], int x, int lo, int hi)
 {
   int mid =  lo + ((hi - lo) / 2);
-
-  cout << "X:   " << x   << "\n"
-       << "MID: " << mid << " " << A[mid]<< "\n"
-       << "LO:  " << lo  << " " << A[lo] << "\n"
-       << "HI:  " << hi  << " " << A[hi] << "\n";
 
   if(mid == hi && A[mid] > x)
     return mid - 1;
@@ -22,48 +16,42 @@ int apartition(int A[], int x, int lo, int hi)
   else if(A[mid-1] <= x && A[mid ] > x)
     return mid - 1;
   else if(x < A[mid])
-    return apartition(A, x, lo, mid - 1);
+    return r_partition(A, x, lo, mid - 1);
   else if(x >= A[mid])
-    return apartition(A, x, mid + 1, hi);
+    return r_partition(A, x, mid + 1, hi);
 }
 
-int r_partition(int A[], const int x, const int sz = 0, const int n = 0)
+int i_partition(int A[], int x, int lo, int hi)
 {
-  int m = (sz / 2) + 1;
-  cout << "x : " << x << " sz : " << sz << " m : " << m
-       <<  " A[m] : " << A[m] << endl;
-  if(m == 1 && A[m] > x) return 0;
-  else if(x > A[n - 1]) return n;
-  else if(A[m-1] <= x && A[m+1] > x) return m;
-  else if(A[m] <= x) m = ((m+1)/2) + 1;// m = m + ((m + 1) / 2) + 1 ;
-  else m = (sz - m)/2;
+  if(x < A[lo])
+    return 0;
+  else if(x == A[lo])
+    return lo;
+  else if(x >= A[hi])
+    return hi;
 
-  return r_partition(A, x, m, n);
-}
+  int q = -1,
+      mid;
 
-int i_partition(int A[], const int x, const int n)
-{
-  int m = -1,
-      i = 1;
-
-  m = (n / 2) + 1;
-
-  cout << "x : " << x << " m : " << m <<  " A[m] : " << A[m] << endl;
-  while(!(A[m] <= x && A[m+1] > x) && m > 0 && m < n )
+  while(q < 0)
   {
-    if(m == 1 && A[m] > x) m = 0;
-    else if(x > A[n - 1]) m = n;
-    else if(A[m] <= x) m = ((m + 1) / 2) + 1 ;
-    else m = ((m - 1) / 2) + 1;
-    cout << "x : " << x << " m : " << m <<  " A[m] : " << A[m] << endl;
+    mid =  lo + ((hi - lo) / 2);
+
+    if(A[mid-1] <= x && A[mid ] > x)
+      q = mid - 1;
+    else if(x < A[mid])
+      hi = mid - 1;
+    else if(x >= A[mid])
+      lo = mid + 1;
   }
-  return m;
+
+  return q;
 }
 
 void test(int A[], const int n)
 {
   cout << "Begin...\n";
-  int    i = 1, q0, q1;
+  int    i = 1, qi, qr;
 
   cout << "A: ";
   for( ; i < n; ++i)
@@ -72,15 +60,12 @@ void test(int A[], const int n)
   cout << "\nTesting Partition...\n";
   for(i = 1; i < 11; ++i)
   {
-    int q = apartition(A, i, 1, n-1);
-    /*cout << "IPartioning...\n";
-    q0 = i_partition(A, i+1, n);
     cout << "RPartioning...\n";
-    q1 = r_partition(A, i+1, n, n);
-    cout << "\nIPartition " << i+1 << " Returned Q: " << q0
-         << "\nRPartition " << i+1 << " Returned Q: " << q1 << endl;
-    */
-    cout << "\nIPartition " << i << " Returned Q: " << q << endl;
+    qr = r_partition(A, i, 1, n-1);
+    cout << "IPartioning...\n";
+    qi = i_partition(A, i, 1, n -1);
+    cout << "\nRPartition " << i << " Returned Q: " << qr << endl;
+    cout << "\nIPartition " << i << " Returned Q: " << qi << endl;
   }
 
   cout << "End...\n";
@@ -93,7 +78,7 @@ int main(int argv, char** argc)
 
   // 1 : 0 | 2 3 :1 | 4 : 2 | 5 6 7 : 2
   test(A0, 7);
-//  test(A1, 8);
+  test(A1, 8);
 
   return 1;
 }
