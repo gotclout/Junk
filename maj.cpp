46b34b7d3719a3f229808a4e0f2dc8daff509258
gotclout <rlfosterjr@gmail.com>
2014-12-24 17:22:06 -0500

    Create maj.cpp
    how are map values initialized? this works but assumes += on initial value is 0.

---
+++ maj.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+const int maxelem = 23; //num elements to generate
+
+/**
+ * Find majority or max occurs of values in elements 
+ * elemements not empty
+ */
+int majority(vector<int> & e)
+{
+  int m  = 0,        //majority value
+      mp = 0;        //majority position
+  map<int, int> kv;  //map key element value occurences
+
+  for(int i = 0; i < maxelem; ++i)
+  {
+    kv[e[i]] += 1;
+    if(kv[e[i]] > m)
+    {
+      m  = kv[e[i]];
+      mp = e[i];
+    }
+  }
+
+  //print
+  cout << "map:\n";
+  for(int j = 0; j < maxelem/2; ++j) cout << j << " -> " << kv[j] << "\n";
+
+  return kv[mp];
+}
+
+int main(int argc, char** argv)
+{
+  srand(time(0));
+  vector<int> v;
+
+  //generate
+  for(int i = 0; i < maxelem; ++i) v.push_back(rand() % (maxelem/2));
+
+  //print
+  cout << "elems: ";
+  for(int j = 0; j < maxelem; ++j) cout << v[j] << " ";
+  cout << "\n";
+
+  //compute majority
+  int maj = majority(v);
+  cout << "Majority Element: " << maj << "\n";
+  
+  return 0;
+}
