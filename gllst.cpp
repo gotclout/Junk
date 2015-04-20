c4fe4459379e5c6628905098ba9d546f5a1e4a3b
General <rlfosterjr@gmail.com>
2015-04-20 16:03:49 -0400

    Create gllst.cpp

---
+++ gllst.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+#include "timer.h"
+
+using namespace std;
+
+//Let N be a constant value for length
+#define N 23
+
+//Let gvals be a global array of N integers
+int gvals[N];
+
+/**
+ * Nodes of a linked list
+ */
+struct lnode
+{
+  void* data;
+  lnode* next;
+
+  lnode() { data = 0; next = 0; };
+};
+
+/**
+ * A linked list of nodes from head to tail
+ */
+struct list
+{
+  lnode* head;
+  lnode* tail;
+
+  /**
+   * Default Constructor initializes list nodes to null
+   */
+  list() { head = tail = 0; };
+
+  /**
+   * List insertion, allocates memory for the size of data to be
+   * stored in the list.
+   *
+   * @param void* data, is the data to be stored in the list lode
+   * @param void* data, is the size of the data to be stored
+   */
+  bool insert(void* data, int size)
+  {
+    bool retVal = true;
+
+    if(data)
+    {
+      lnode* n = new lnode();
+      n->data = (void*) new char[size];
+      memset(n->data, 0, size);
+      memcpy(n->data, data, size);
+      if(!head)
+        head = tail = n;
+      else
+      {
+        tail->next = n;
+        tail = n;
+      }
+    }
+    else
+      retVal = false;
+
+    return retVal;
+  };
+
+  /**
+   * List Destructor
+   * Deallocates memory allocated by calling insert
+   */
+  ~list()
+  {
+    lnode* curr = head, *tmp = 0;
+    while(curr)
+    {
+      if(curr->data)
+      {
+        char* d = (char*) curr->data;
+        delete [] d;
+        curr->data = 0;
+      }
+      tmp = curr;
+      curr = curr->next;
+      delete tmp;
+      tmp = 0;
+    }
+  };
+};
+
+/**
+ * Populate the gvals array with random values between 1 and N
+ */
+void initvals()
+{
+    for(int i = 0; i < N; i++)
+      gvals[i] = rand() % N + 1;
+}
+
+/**
+ * Test the list by creating, insert values, print values, and destroying.
+ */
+void testlist()
+{
+  list* l = new list();
+  for(int i = 0; i < N; i++)
+    l->insert(&gvals[i], sizeof(int));
+
+  lnode* curr = l->head;
+
+  while(curr)
+  {
+    cout << *((int*)(curr->data)) << endl;
+    curr = curr->next;
+  }
+
+  delete l;
+}
+
+/**
+ * Seed the random number generator;
+ * Generate a set of global integers
+ * Test the list
+ */
+int main(int argc, char** argv)
+{
+  timer t;
+
+  t.Start();
+
+  srand(time(0));
+  initvals();
+  testlist();
+
+  t.Stop();
+
+  cout << "Time: " << t.Duration() << endl;
+  return 0;
+}
